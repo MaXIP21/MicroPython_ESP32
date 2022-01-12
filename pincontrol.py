81c4ba709b2f696736950231e0a2e4688601328e
Peter Bacsai <peter.bacsai@gmail.com>
2022-01-12 21:29:39 +0100

    Commit correcting pincontrol

--- pincontrol.py
+++ pincontrol.py
@@ -1,132 +1,121 @@
-
-import machine, display, time, math, network, utime, gc
-try:
-  import usocket as socket
-except:
-  import socket
-import _thread
 from machine import Pin
+import time
+import utime
+import _thread
 
-
-
-class pc_control:
+class pinControl:
     def __init__(self):
-      self.tft = display.TFT()
-      self.status_timeout=3
-      self.ledstate={}
-      self.interrupt = False
-      self.data = {}
-      self.refresh_time = 1
-      self.display = 1
-      self.update = False
-      self.keypress_time={'35':0, '0':0}
-    gc.collect()
-    
-    
-    def update_led_value(self, pin, value):
-      self.ledstate["led"+str(pin)]=value
-      
-    def send_message_to_tft(self, key, value):
+        self.resetpin=15
+        self.powerpin=13
+        self.pinstatus = {}
+        self.pindefault=Pin.INOUT
+        self.pindefaultstate=Pin.PULL_UP
+        self.resetBtnLength=1
+        self.powerBtnLength=6
+        self.interrupt=False
+        self.refresh_time=10
+        self.pinDictionary = { "resetpin": {"pin":2, "state":0, "type":Pin.INOUT }, "powerpin":{"pin" : 15, "state" : 0, "type":Pin.INOUT } }
+        
+    @classmethod
+    def pintoggle(self, pin):
+        if pin.value() == 1:
+            print(0)
+            pin.value(0)
+        else:
+            print(1)
+            pin.value(1)
 
-      self.data[key]=value
-      self.update = True
-      
-    def send_hw_push(sec=1):
-      self.led2.value(1)
-      time.sleep(sec)
-      self.led2.value(0)
-      self.update_led_value(2, 0)
-      
-      
-    def toggle_display(self):
-      if self.display == 0 :
-        self.display = 1
-        self.update = True
-        self.initialize_display()
-        print("Turning on display..")
+    def push_button(self, pin, seconds):
+        if pin.value() == 1:
+            pin.value(0)
+            time.sleep(seconds)
+            pin.value(1)
+        else:
+            pin.value(1)
+            time.sleep(seconds)
+            pin.value(0)
+            
+    def update_pin_state(self, pinName, state):
+      if pinName in self.pinDictionary.keys():
+        currentValue=self.pinDictionary[pinName]
+        currentValue["state"]=state
+        self.pinDictionary[pinName]=currentValue
+        self.get_pin_parameters(pinName)
       else:
-        self.display = 0
-        self.tft.clear()
-        self.initialize_display(0)
-        self.tft.deinit()
-        print("Turning off display..")
+        print("Key not found in the dictionary!")
         
-    def pin0_handle_interrupt(self, pin):
-      start = time.ticks_us()
-      diff = start-self.keypress_time['0']
-      if diff > 5000000:
-        print("Button 0 pressed..")
-        self.keypress_time['0']=time.ticks_us()
-        #self.classpinControl.press_pin("resetpin")
-        try:
-          _thread.sendmsg(0, "pressReset")
-        except Exception as e:
-          print(e)
-          pass
-          
+    def get_pin_parameters(self, pinName):
+      if pinName in self.pinDictionary.keys():
+        print(self.pinDictionary[pinName])
       else:
-        #print("Button 0 lag."+str(diff))
+        print("Key not found in the dictionary!")
+        
+    def toggle_pin(self, pinName):
+      if pinName == "resetpin":
+        print("Pressing reset button")
+        self.pintoggle(self.resetPinInstance)
+        self.set_pin(self.resetpin, self.resetPinInstance.value())
+      elif pinName == "rebootpin":
+        self.pintoggle(self.powerPinInstance)
+        self.set_pin(self.powerpin, self.powerPinInstance.value())
+      else:
+        print("Unknown pin : "+pinName)
         pass
         
+    def press_pin(self, pinName):
+      if pinName == "resetpin":
+        print("Pressing reset button for "+str(self.resetBtnLength)+" seconds.")
+
+        self.push_button(self.resetPinInstance, self.resetBtnLength)
+        self.set_pin(self.resetpin, self.resetPinInstance.value())
+      elif pinName == "rebootpin":
+        print("Pressing power button for "+str(self.powerBtnLength)+" seconds.")
+
+        self.push_button(self.powerPinInstance, self.powerBtnLength)
+        self.set_pin(self.powerpin, self.powerPinInstance.value())
+      else:
+        print("Unknown pin : "+pinName)
+        pass
         
+    def set_pin(self, pin, value):
+        self.pinstatus[pin] = value
+
+    def initialize_pins(self):
+        self.resetPinInstance=Pin(self.resetpin, self.pindefault, self.pindefaultstate)
+        self.powerPinInstance=Pin(self.powerpin, self.pindefault, self.pindefaultstate)
+
+    def get_status_of_pins(self):
+        self.set_pin(self.resetpin, self.resetPinInstance.value())
+        self.set_pin(self.powerpin, self.powerPinInstance.value())
     
-    def pin35_handle_interrupt(self, pin):
-      start = time.ticks_us()
-      diff = start-self.keypress_time['35']
-      if diff > 1000000:
-        #print("Button 35 pressed..")
-        self.toggle_display()
-        self.keypress_time['35']=time.ticks_us()
-      else:
-        #print("Button 35 lag."+str(diff))
-        pass
-        
+    def Convert(self,string):
+      self.li = list(string.split(","))
+      return self.li
       
-    def initialize_display(self, backl=1):
-      self.tft.init(self.tft.ST7789,bgr=False,rot=self.tft.LANDSCAPE, miso=17,backl_pin=4,backl_on=backl, mosi=19, clk=18, cs=5, dc=16)
-      self.tft.setwin(40,52,320,240)
-      self.tft.set_fg(0x000000)
-      self.tft.set_bg(0xFFFFFF)
-      self.tft.clear()
-      
-    def empty_buffer(self):
-      self.data = {}
-      
-    def clear_display(self, sleeptime=0):
-      time.sleep(sleeptime)
-      self.tft.clear()
-      
-    def print_status(self):
-      self.tft.clear()
-      self.tft.text(5,5,"HELLO")
-      
-    def write_to_tft(self,data):
-      #self.tft.clear()
-      if "line1" in data and data["line1"] != "":
-        self.tft.text(5,5,data["line1"])
-      if "line2" in data and data["line2"] != "":
-        self.tft.text(5,20,data["line2"])
-      if "line3" in data and data["line3"] != "":
-        self.tft.text(5,35,data["line3"])
-      if "status" in data:
-        if data["status"] != "":
-          self.tft.text(5,35,"Status : "+data["status"])
-          data["status"] = ""
-          time.sleep(self.status_timeout)
-          self.write_to_tft(data)
-      self.update = False
-          
-    def monitor_data(self):
-      self.initialize_display()
-      self.clear_display(0)
-      while self.interrupt != True:
-        time.sleep(self.refresh_time)
-        if self.update:
-          self.write_to_tft(self.data)
-        
-    def activate_button(self):
-      self.button1=Pin(35, Pin.IN)
-      self.button2=Pin(0, Pin.IN, Pin.PULL_UP)
-      self.button1.init(trigger=Pin.IRQ_RISING, handler=self.pin35_handle_interrupt, debounce=1500, acttime=400)
-      self.button2.init(trigger=Pin.IRQ_RISING, handler=self.pin0_handle_interrupt, debounce=1500, acttime=400)
+    def startPinThread(self):
+        print("TH_FUNC: started")
+        _thread.allowsuspend(True)
+        self.initialize_pins()
+        while True:
+            ntf = _thread.getnotification()
+            if ntf:
+                if ntf == _thread.EXIT:
+                    print("TH_FUNC: terminated")
+                    return
+                elif ntf == _thread.SUSPEND:
+                    print("TH_FUNC: suspended")
+                    while _thread.wait() != _thread.RESUME:
+                        pass
+                else:
+                    pass
+            ntf = _thread.wait(1000)
+            typ, sender, msg = _thread.getmsg()
+            if msg:
+                # Reply to sender, we can analyze the message first
+                _thread.sendmsg(sender, "[%s] Hi %s, received your message." % (_thread.getSelfName(), _thread.getThreadName(sender)))
+                if type(self.Convert(msg)[0] == str):
+                  if self.Convert(msg)[0] == "pressReset":
+                    self.press_pin("resetpin")
+                  elif self.Convert(msg)[0] == "pressPower":
+                    self.press_pin("rebootpin")
 
